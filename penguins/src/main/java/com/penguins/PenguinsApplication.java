b7b02996989d28d3a348f184f97e07dee76f4b17
zhengbing <mydreambing@126.com>
2022-05-07 16:04:07 +0800

    init commit

---
+++ penguins/src/main/java/com/penguins/PenguinsApplication.java
@@ -0,0 +1,13 @@
+package com.penguins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenguinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PenguinsApplication.class, args);
+    }
+
+}
---
+++ penguins/src/test/java/com/penguins/PenguinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.penguins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PenguinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
