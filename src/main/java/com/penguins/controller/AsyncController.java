3f7a6a41dcff8f15386642809ec46726a7fd1a78
zhengbinger <mydreambing@126.com>
2022-07-05 09:21:18 +0800

    rebuild

--- src/main/java/com/penguins/controller/AsyncController.java
+++ src/main/java/com/penguins/controller/AsyncController.java
@@ -3,6 +3,7 @@ package com.penguins.controller;
 import com.penguins.service.AdminRoleService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +22,7 @@ public class AsyncController {
     @Resource
     private AdminRoleService adminRoleService;
 
-    @RequestMapping("test")
+    @GetMapping("test")
     @ApiOperation("异步执行任务")
     public void async() {
         adminRoleService.task();
--- src/main/java/com/penguins/controller/TestController.java
+++ src/main/java/com/penguins/controller/TestController.java
@@ -1,50 +1,50 @@
-package com.penguins.controller;
-
-import com.penguins.component.mail.MailSenderFactory;
-import com.penguins.component.mail.MailType;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import javax.annotation.Resource;
-
-/**
- * @author 郑冰
- * @date 2022/5/17 16:49
- * @email mydreambing@126.com
- * @since jdk8
- **/
-
-@Api(tags = "测试接口")
-//@RequestMapping("test")
-@Controller
-public class TestController {
-
-    @Resource
-    private MailSenderFactory mailSenderFactory;
-
-    @ApiOperation("跳转登录页面")
-    @RequestMapping(value = "login", method = RequestMethod.GET)
-    public String login() {
-        return "login";
-    }
-
-    @ApiOperation("跳转首页")
-    @RequestMapping(value = "index", method = RequestMethod.GET)
-    public String index() {
-        return "index";
-    }
-
-    @GetMapping
-    public void sendMail() {
-        mailSenderFactory.getSender(MailType.TEXT).send(null);
-    }
-
-    @GetMapping("temp")
-    public void sendMailTemplate() {
-        mailSenderFactory.getSender(MailType.TEMPLATE).send(null);
-    }
-}
+//package com.penguins.controller;
+//
+//import com.penguins.component.mail.MailSenderFactory;
+//import com.penguins.component.mail.MailType;
+//import io.swagger.annotations.Api;
+//import io.swagger.annotations.ApiOperation;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//
+//import javax.annotation.Resource;
+//
+///**
+// * @author 郑冰
+// * @date 2022/5/17 16:49
+// * @email mydreambing@126.com
+// * @since jdk8
+// **/
+//
+//@Api(tags = "测试接口")
+////@RequestMapping("test")
+//@Controller
+//public class TestController {
+//
+//    @Resource
+//    private MailSenderFactory mailSenderFactory;
+//
+//    @ApiOperation("跳转登录页面")
+//    @RequestMapping(value = "login", method = RequestMethod.GET)
+//    public String login() {
+//        return "login";
+//    }
+//
+//    @ApiOperation("跳转首页")
+//    @RequestMapping(value = "index", method = RequestMethod.GET)
+//    public String index() {
+//        return "index";
+//    }
+//
+//    @GetMapping
+//    public void sendMail() {
+//        mailSenderFactory.getSender(MailType.TEXT).send(null);
+//    }
+//
+//    @GetMapping("temp")
+//    public void sendMailTemplate() {
+//        mailSenderFactory.getSender(MailType.TEMPLATE).send(null);
+//    }
+//}
